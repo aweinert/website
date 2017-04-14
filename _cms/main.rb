24944a13423e46be758f1316ab375aa477a4b6e2
Alexander Weinert <weinert@react.uni-saarland.de>
2017-04-14 12:34:58 +0200

    begin structuring

--- _cms/main.rb
+++ _cms/main.rb
@@ -4,121 +4,122 @@ require 'fastimage'
 require 'highline/import'
 
 
-$config = {
-	:thumb => {
-		:suffix => "thumb",
-		:watermark => nil,
-		:quality => 90,
-		:longest_edge => 300
-	},
-	:alexanderweinert => {
-		:suffix => "aw",
-		:watermark => "wm-aw.png",
-		:quality => 90,
-		:longest_edge => 1000
-	},
-	:facebook => {
-		:suffix => "fb",
-		:watermark => "wm-fb.png",
-		:quality => 90,
-		:longest_edge => 1000
-	},
-	:fivehundredpx => {
-		:suffix => "500px",
-		:watermark => "wm-500px.png",
-		:quality => 90,
-		:longest_edge => 1000
-	}
-}
 
 
-def add_watermark(input, watermark, wm_position, target)
-	system("composite -watermark 30% -gravity southeast -geometry #{wm_position[:width]}x+#{wm_position[:offset_x]}+#{wm_position[:offset_y]} #{watermark[:path]} #{input[:path]} #{target}")
-end
-
-def copy_file(from, to)
-	system("cp #{from} #{to}")
-end
-
-def get_temp_path()
-	return "temp.jpg"
-end
-
-def resize_and_compress(from, longest_edge, quality, to)
-	system("convert #{from} -resize #{longest_edge}x#{longest_edge} -quality #{quality}% #{to}")
-end
-
-def parse_image(path)
-	size = FastImage.size(path)
-	return { :path => path, :width => size[0], :height => size[1] }
-end
-
-def is_landscape(image)
-	# We treat square photos as portraits
-	return image[:width] > image[:height]
-end
-
-def get_wm_position(watermark, image)
-	def landscape(watermark, image)
-		wm_target_width = image[:width] / 4.0
-		wm_target_height = wm_target_width / watermark[:width] * watermark[:height]
-		wm_target_offset_x = image[:width] * 1 / 8
-		wm_target_offset_y = wm_target_height * 1
-		return { :width => wm_target_width.to_i, :offset_x => wm_target_offset_x.to_i, :offset_y => wm_target_offset_y.to_i }
+class Photography
+	def initialize()
+		@config = {
+			:thumb => {
+				:suffix => "thumb",
+				:watermark => nil,
+				:quality => 90,
+				:longest_edge => 300
+			},
+			:alexanderweinert => {
+				:suffix => "aw",
+				:watermark => "wm-aw.png",
+				:quality => 90,
+				:longest_edge => 1000
+			},
+			:facebook => {
+				:suffix => "fb",
+				:watermark => "wm-fb.png",
+				:quality => 90,
+				:longest_edge => 1000
+			},
+			:fivehundredpx => {
+				:suffix => "500px",
+				:watermark => "wm-500px.png",
+				:quality => 90,
+				:longest_edge => 1000
+			}
+		}
 	end
 
-	def portrait(watermark, image)
-		wm_target_width = image[:width] / 3.5
-		wm_target_offset_x = image[:width] * 1 / 4
-		wm_target_offset_y = image[:height] * 1 / 5
-		return { :width => wm_target_width.to_i, :offset_x => wm_target_offset_x.to_i, :offset_y => wm_target_offset_y.to_i }
+	def add_watermark(input, watermark, wm_position, target)
+		system("composite -watermark 30% -gravity southeast -geometry #{wm_position[:width]}x+#{wm_position[:offset_x]}+#{wm_position[:offset_y]} #{watermark[:path]} #{input[:path]} #{target}")
 	end
 
-	if is_landscape(image)
-		return landscape(watermark, image)
-	else
-		return portrait(watermark, image)
+	def copy_file(from, to)
+		system("cp #{from} #{to}")
 	end
-end
 
-def make_dir(name)
-	system("mkdir #{name}")
-end
+	def get_temp_path()
+		return "temp.jpg"
+	end
 
-def remove_file(path)
-	system("rm #{path}")
-end
+	def resize_and_compress(from, longest_edge, quality, to)
+		system("convert #{from} -resize #{longest_edge}x#{longest_edge} -quality #{quality}% #{to}")
+	end
 
-def watermark(path, id)
-	image = parse_image(path)
+	def parse_image(path)
+		size = FastImage.size(path)
+		return { :path => path, :width => size[0], :height => size[1] }
+	end
 
-	basename = id
-	make_dir(basename)
-	retval = {}
-	$config.each do |pair|
-		current_config = pair[1]
+	def is_landscape(image)
+		# We treat square photos as portraits
+		return image[:width] > image[:height]
+	end
 
-		temp_path = get_temp_path()
-		if current_config[:watermark] != nil
-			watermark = parse_image(current_config[:watermark])
-			wm_position = get_wm_position(watermark, image)
-			add_watermark(image, watermark, wm_position, temp_path)
-		else 
-			copy_file(image[:path], temp_path)
+	def get_wm_position(watermark, image)
+		def landscape(watermark, image)
+			wm_target_width = image[:width] / 4.0
+			wm_target_height = wm_target_width / watermark[:width] * watermark[:height]
+			wm_target_offset_x = image[:width] * 1 / 8
+			wm_target_offset_y = wm_target_height * 1
+			return { :width => wm_target_width.to_i, :offset_x => wm_target_offset_x.to_i, :offset_y => wm_target_offset_y.to_i }
 		end
 
-		target_path = "#{basename}/#{basename}-#{current_config[:suffix]}.jpg"
-		resize_and_compress(temp_path, current_config[:longest_edge],current_config[:quality], target_path)
-		remove_file(temp_path)
-		retval[pair[0]] = target_path
+		def portrait(watermark, image)
+			wm_target_width = image[:width] / 3.5
+			wm_target_offset_x = image[:width] * 1 / 4
+			wm_target_offset_y = image[:height] * 1 / 5
+			return { :width => wm_target_width.to_i, :offset_x => wm_target_offset_x.to_i, :offset_y => wm_target_offset_y.to_i }
+		end
+
+		if is_landscape(image)
+			return landscape(watermark, image)
+		else
+			return portrait(watermark, image)
+		end
 	end
-	return retval
-end
 
-choose do |menu|
-	menu.prompt = "What would you like to edit?"
+	def make_dir(name)
+		system("mkdir #{name}")
+	end
 
-	menu.choices(:Photography) { 
+	def remove_file(path)
+		system("rm #{path}")
+	end
+
+	def watermark(path, id)
+		image = parse_image(path)
+
+		basename = id
+		make_dir(basename)
+		retval = {}
+		@config.each do |pair|
+			current_config = pair[1]
+
+			temp_path = get_temp_path()
+			if current_config[:watermark] != nil
+				watermark = parse_image(current_config[:watermark])
+				wm_position = get_wm_position(watermark, image)
+				add_watermark(image, watermark, wm_position, temp_path)
+			else 
+				copy_file(image[:path], temp_path)
+			end
+
+			target_path = "#{basename}/#{basename}-#{current_config[:suffix]}.jpg"
+			resize_and_compress(temp_path, current_config[:longest_edge],current_config[:quality], target_path)
+			remove_file(temp_path)
+			retval[pair[0]] = target_path
+		end
+		return retval
+	end
+
+	def main()
 		id = ask("ID? (short name that the photograph is referred to internally)")
 		title = ask("Title? (shown below the photograph)")
 		path = ask("Path to the original photograph?")
@@ -135,5 +136,17 @@ choose do |menu|
 
 		copy_file(temp_path, "../_data/photography.yml")
 		remove_file(temp_path)
-	}
+	end
 end
+
+def main()
+	choose do |menu|
+		menu.prompt = "What would you like to edit?"
+
+		menu.choices(:Photography) { 
+			Photography.new().main
+		}
+	end
+end
+
+main()
