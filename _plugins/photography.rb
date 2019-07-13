9649471ea38609338fab0c3a9fe7058117bbe5ba
Alexander Weinert <weinert@react.uni-saarland.de>
2019-07-13 10:25:34 +0200

    Clean up photography plugin

--- _plugins/photography.rb
+++ _plugins/photography.rb
@@ -3,28 +3,36 @@ include Magick
 
 $photos = []
 
-module Jekyll
-	module PhotoPortfolioFilter
-		def photo_path_full(photo_id)
-			$photos.push(photo_id)
-			"/assets/img/thumb/#{photo_id}"
+module AlexanderWeinertNet
+	module Photography
+		module Filter
+			def photo_path_full(photo_id)
+				$photos.push(photo_id)
+				"/assets/img/thumb/#{photo_id}"
+			end
+
+			def photo_path_thumb(photo_id)
+				$photos.push(photo_id)
+				"/assets/img/full/#{photo_id}"
+			end
 		end
 
-		def photo_path_thumb(photo_id)
-			$photos.push(photo_id)
-			"/assets/img/full/#{photo_id}"
+		def post_site_write(site, hash)
+			$photos.each do |path|
+				image = ImageList.new("_assets/img/#{path}").first
+				image.resize_to_fit(1000, 1000).write("_site/assets/img/full/#{path}")
+
+				image = ImageList.new("_assets/img/#{path}").first
+				image.resize_to_fit(300, 300).write("_site/assets/img/thumb/#{path}")
+			end
 		end
 	end
 end
 
-Liquid::Template.register_filter(Jekyll::PhotoPortfolioFilter)
+include AlexanderWeinertNet::Photography
+
+Liquid::Template.register_filter(AlexanderWeinertNet::Photography::Filter)
 
 Jekyll::Hooks.register :site, :post_write { |site, hash|
-	$photos.each do |path|
-		image = ImageList.new("_assets/img/#{path}").first
-		image.resize_to_fit(1000, 1000).write("_site/assets/img/full/#{path}")
-
-		image = ImageList.new("_assets/img/#{path}").first
-		image.resize_to_fit(300, 300).write("_site/assets/img/thumb/#{path}")
-	end
+	AlexanderWeinertNet::Photography::post_site_write(site, hash)
 }
