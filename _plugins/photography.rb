9a302c3833bba59ef55ed2306cc7bc892f43a031
Alexander Weinert <weinert@react.uni-saarland.de>
2019-07-20 07:27:06 +0200

    Pull logging out of photography plugin

--- _plugins/photography.rb
+++ _plugins/photography.rb
@@ -1,29 +1,89 @@
 require 'RMagick'
+require 'Set'
 include Magick
+require 'yaml'
 
-$photos = []
+require_relative '../_script_includes/logging'
+include Logging
+
+$photos = Set.new
+
+def get_full_path_display(photo_id); "_assets/img/#{photo_id}".underline end
+def get_scaled_paths_display(photo_id); "assets/img/{full,thumb}/#{photo_id}".underline end
+def get_scaled_full_path_display(photo_id); "assets/img/full/#{photo_id}".underline end
+def get_scaled_thumb_path_display(photo_id); "assets/img/thumb/#{photo_id}".underline end
+
+YAML_FILE = '_data/photography.yml'
+
 
 module AlexanderWeinertNet
 	module Photography
 		module Filter
 			def photo_path_full(photo_id)
-				$photos.push(photo_id)
-				"/assets/img/thumb/#{photo_id}"
+				$photos.add(photo_id)
+				"/assets/img/full/#{photo_id}"
 			end
 
 			def photo_path_thumb(photo_id)
-				$photos.push(photo_id)
-				"/assets/img/full/#{photo_id}"
+				$photos.add(photo_id)
+				"/assets/img/thumb/#{photo_id}"
 			end
+		end
+
+		def post_site_read(site, hash)
+
+			data = YAML.load_file(YAML_FILE)
+
+			report_status("Loaded #{YAML_FILE}")
+
+			report_status("Checking syntax of #{YAML_FILE}")
+			if not data.kind_of?(Array) then
+				report_error("#{YAML_FILE} does not contain an array at top level")
+			end
+
+			abort_if_failure()
+
+			data.each { |item| 
+				if not item.kind_of?(Hash) then
+					report_failure("Entry #{item} is not a dictionary")
+					next
+				end
+
+				if not item.key?('id') then
+					report_failure("Entry #{item} does not contain a key 'path'")
+				end
+
+				if not item['id'].kind_of?(String) then
+					report_failure("Item #{item}: Value for key 'id' is not a String, but a #{item['path'].class}")
+				end
+
+				if not item.key?('title') then
+					report_failure("Entry #{item} does not contain a key 'title'")
+				end
+
+				if not item['title'].kind_of?(String) then
+					report_failure("Item #{item}: Value for key 'title' is not a String, but a #{item['path'].class}")
+				end
+
+				actual_keys = Set.new item.keys
+				expected_keys = Set.new ['id', 'title']
+
+				if not actual_keys == expected_keys then
+					report_warning("Item #{item} contains unexpected keys: #{(actual_keys - expected_keys).inspect()}")
+				end
+			}
+
+			report_success("#{YAML_FILE} is syntactically correct")
+
 		end
 
 		def post_site_write(site, hash)
 			$photos.each do |path|
 				image = ImageList.new("_assets/img/#{path}").first
-				image.resize_to_fit(1000, 1000).write("_site/assets/img/full/#{path}")
+				image.resize_to_fit(1000, 1000).write("_site/assets/img/full/#{path}") { self.quality = 90 }
 
 				image = ImageList.new("_assets/img/#{path}").first
-				image.resize_to_fit(300, 300).write("_site/assets/img/thumb/#{path}")
+				image.resize_to_fit(360, 360).write("_site/assets/img/thumb/#{path}") { self.quality = 90 }
 			end
 		end
 	end
@@ -32,6 +92,10 @@ end
 include AlexanderWeinertNet::Photography
 
 Liquid::Template.register_filter(AlexanderWeinertNet::Photography::Filter)
+
+Jekyll::Hooks.register :site, :post_read { |site, hash|
+	AlexanderWeinertNet::Photography::post_site_read(site, hash)
+}
 
 Jekyll::Hooks.register :site, :post_write { |site, hash|
 	AlexanderWeinertNet::Photography::post_site_write(site, hash)
---
+++ _script_includes/logging.rb
@@ -0,0 +1,57 @@
+module Logging
+
+$successes_reported = 0
+$warnings_reported = 0
+$failures_reported = 0
+
+# Taken from https://stackoverflow.com/a/16363159
+class String
+	def red;            "\e[31m#{self}\e[0m" end
+	def green;          "\e[32m#{self}\e[0m" end
+	def yellow;         "\e[33m#{self}\e[0m" end
+	def underline;      "\e[4m#{self}\e[24m" end
+	def reverse_color;  "\e[7m#{self}\e[27m" end
+end
+
+def assert_or_failure(condition, success_message, error_message)
+	if condition.call then
+		report_success(success_message)
+	else
+		report_failure(error_message)
+	end
+end
+
+def assert_or_warning(condition, success_message, warning_message)
+	if condition.call then
+		report_success(success_message)
+	else
+		report_warning(warning_message)
+	end
+end
+
+def report_status(message)
+    puts message
+end
+
+def report_success(message)
+	puts "    ✅ #{message}".green
+	$successes_reported += 1
+end
+
+def report_warning(message)
+	puts "    ⚠️  #{message}".yellow
+	$warnings_reported += 1
+end
+
+def report_failure(message)
+	puts "    ❌ #{message}".red
+	$failures_reported += 1
+end
+
+def abort_if_failure()
+	if $failure_reported then
+		exit(false)
+	end
+end
+
+end
